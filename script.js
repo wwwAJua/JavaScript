e0a2fbb19aa10b233c461dc403bf83fd2af4456e
Anton io <73609544+wwwAJua@users.noreply.github.com>
2023-09-02 20:56:54 +0300

    Содержимое элементов
    Способы получения доступа к элементам и их содержимому

--- script.js
+++ script.js
@@ -9,9 +9,20 @@ var p = document.querySelectorAll('.text')
 var h1 = document.querySelector('h1')
 var ul1 = document.querySelector('#playground ul')
 var ul2 = document.querySelector('#playground > div ul')
+var input = document.querySelector('input')
 
-console.log(div)
-console.log(p)
-console.log(h1)
-console.log(ul1)
-console.log(ul2)
+// console.log(div)
+// console.log(p)
+// console.log(h1)
+// console.log(ul1)
+// console.log(ul2)
+
+// Содержимое элементов
+
+h1.textContent = 'Changed from JavaScript'
+input.value = 'I was input, а стал инпутом'
+
+console.log(div.innerHTML)
+console.log(h1.textContent)
+console.log(input.value)
+
