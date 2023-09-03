6382a848c80b0066d73e5f7ba766775b02a89b4e
Anton io <73609544+wwwAJua@users.noreply.github.com>
2023-09-03 21:49:21 +0300

    Атрибуты

--- script.js
+++ script.js
@@ -1,28 +1,11 @@
+// Атрибуты
 
-// Доступ к элементам DOM (.getElement -> .querySelector)
+var a = document.querySelector('a')
+var oldHref = a.getAttribute('href')
+a.setAttribute('href', 'https://youtube.com/')
+a.textContent = 'Youtube'
+a.setAttribute('title', 'clik me')
 
-// var div = document.getElementById('playground')
-// var p = document.getElementsByClassName('text')
-
-var div = document.querySelector('#playground')
-var p = document.querySelectorAll('.text')
-var h1 = document.querySelector('h1')
-var ul1 = document.querySelector('#playground ul')
-var ul2 = document.querySelector('#playground > div ul')
-var input = document.querySelector('input')
-
-// console.log(div)
-// console.log(p)
-// console.log(h1)
-// console.log(ul1)
-// console.log(ul2)
-
-// Содержимое элементов
-
-h1.textContent = 'Changed from JavaScript'
-input.value = 'I was input, а стал инпутом'
-
-console.log(div.innerHTML)
-console.log(h1.textContent)
-console.log(input.value)
-
+console.log(a)
+console.log(a.attributes)
+console.log(a.getAttribute('href'))
